c48815fbb259d7493460c454e5fc5ed11b7b186e
saerdna <zhaodahao@gmail.com>
2015-01-13 00:13:26 +0800

    relase version 1.0

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import urllib
 import random
 import json
 import pprint
+import sqlite3
+import time
 
 def luhn_residue(digits):
     return sum(sum(divmod(int(d)*(1 + i%2), 10)) for i, d in enumerate(digits[::-1])) % 10
@@ -23,9 +25,11 @@ class MMSpider:
         self._mmid = mmid
         self._headers = {"Content-Type": "application/x-www-form-urlencoded", "Connection": "Keep-Alive"}
         self._login_url = "https://open.taou.com/maimai/user/v3/login"
-        self._degree_one_url = "https://open.taou.com/maimai/contact/v3/rece_d1"
         self._feed_url = "https://open.taou.com/maimai/contact/v3/feed"
-
+        self._detail_url = "https://open.taou.com/maimai/contact/v3/detail2"
+    def show_info(self):
+        self._login()
+        print >> sys.stderr, "mmid:%s\ttoken:%s" % (self._mmid, self._token)
     def _login(self):
         if self._token != None and self._mmid != None:
             return
@@ -38,24 +42,70 @@ class MMSpider:
         resp = json.loads(req.text)
         self._token = resp['token']
         self._mmid = resp['user']['mmid']
-    def get_degree_one(self):
+    def get_degree(self, dist, pageno):
         self._login()
-        query = {'u': self._mmid, 'access_token': self._token, 'rece': 1, 'version':"3.10.8", 'ver_code':'android_955', 'channel':'XiaoMi'}
-        url = "%(url)s?%(query)s" % ({'url': self._degree_one_url, 'query': urllib.urlencode(query)})
-        headers = self._headers
-        headers["User-Agent"] = self._useragent
-        headers["Charset"] = "UTF-8"
-        req = requests.get(url, headers = headers, verify = False)
-        resp = json.loads(req.text)
-    def get_degree_two(self):
-        self._login()
-        query = {'u': self._mmid, 'access_token': self._token, 'version':"3.10.8", 'ver_code':'android_955', 'channel':'XiaoMi', 'gl': 1, 'count':20, 'page':23, 'gtype':'a', 'dist':2}
+        query = {'u': self._mmid, 'access_token': self._token, 'version':"3.10.8", 'ver_code':'android_955', 'channel':'XiaoMi', 'gl': 1, 'count':20, 'page': pageno, 'gtype':'a', 'dist':dist}
         url = "%(url)s?%(query)s" % ({'url': self._feed_url, 'query': urllib.urlencode(query)})
         headers = self._headers
         headers["User-Agent"] = self._useragent
         headers["Charset"] = "UTF-8"
         req = requests.get(url, headers = headers, verify = False)
         resp = json.loads(req.text)
-        for one in resp['contacts']:
-            for k,v in one.items():
-                print k,v
+        return resp
+    def get_detail(self, uid):
+        self._login()
+        query = {'u': self._mmid, 'access_token': self._token, 'version':"3.10.8", 'ver_code':'android_955', 'channel':'XiaoMi', 'u2': uid}
+        headers = self._headers
+        headers["User-Agent"] = self._useragent
+        headers["Charset"] = "UTF-8"
+        url = "%(url)s?%(query)s" % ({'url': self._detail_url, 'query': urllib.urlencode(query)})
+        req = requests.get(url, headers = headers, verify = False)
+        resp = json.loads(req.text)
+        return resp
+if __name__ == '__main__':
+    print >> sys.stderr, "please input your mobile phone"
+    uid = sys.stdin.readline().strip()
+    print >> sys.stderr, "please input your password"
+    password = sys.stdin.readline().strip()
+    spider = MMSpider(uid, password)
+    relation_one = spider.get_degree(1, 0)
+    conn = sqlite3.Connection("maimai.db")
+    cur = conn.cursor()
+    try:
+        cur.execute('''create table user_info(record_id integer PRIMARY KEY autoincrement, mmid text unique, json text)''')
+    except Exception as Exc:
+        pass
+    page_no = 1
+    while relation_one.get('remain', 0) > 0:
+        for one in relation_one['contacts']:
+            if one['mmid'].isdigit() == False:
+                continue
+            print >> sys.stderr, one['name'], relation_one.get('remain', 0)
+            detail = spider.get_detail(one['mmid'])
+            time.sleep(random.randint(5, 10))
+            try:
+                cur.execute("insert into user_info(mmid, json) values('%s', '%s')" % (one['mmid'], json.dumps(detail)))
+                conn.commit()
+            except Exception as Exc:
+                print >> sys.stderr, Exc
+        relation_one = spider.get_degree(1, page_no)
+        page_no += 1
+
+    relation_two = spider.get_degree(2, 0)
+    page_no = 1
+    while relation_two.get('remain', 0) > 0:
+        for one in relation_two['contacts']:
+            if one['mmid'].isdigit() == False:
+                continue
+            print >> sys.stderr, one['name'], relation_two.get('remain', 0)
+            detail = spider.get_detail(one['mmid'])
+            time.sleep(random.randint(5, 10))
+            try:
+                cur.execute("insert into user_info(mmid, json) values('%s', '%s')" % (one['mmid'], json.dumps(detail)))
+                conn.commit()
+            except Exception as Exc:
+                print >> sys.stderr, Exc
+        relation_two = spider.get_degree(2, page_no)
+        page_no += 1
+
+    conn.close()
